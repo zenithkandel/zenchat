c999715a872fdcc5ac6493bac99d83eefa265daa
Zenith Kandel <kandelze123@gmail.com>
2026-08-27 19:22:13 +0545

    feat: integrate Expo and configure platform permissions and metadata

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+try {
+  const { registerRootComponent } = require('expo');
+  registerRootComponent(App);
+} catch {
+  AppRegistry.registerComponent(appName, () => App);
+}
